c5654fe04b59bd531ee9ab508297685990c5fcc9
Darllan <darllangomes10@hotmail.com>
2025-02-23 09:09:33 -0300

    first commit: criação de projeto e criação da pasta Models

---
+++ Models/Produto.cs
@@ -0,0 +1,8 @@
+namespace ApiCrud.Models
+{
+    public class Produto {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
