d4d2d020cc20c332925aa6470489cd124a37844f
Darllan <darllangomes10@hotmail.com>
2025-03-04 18:33:42 -0300

    configurando docker com postgresql

--- obj/Debug/net8.0/ApiCrud.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiCrud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCrud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+971819b3560b54e3d92c53d8acc70370ff317550")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f337af7b5bdec01cc585b57857349134a1a9bb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCrud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCrud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
